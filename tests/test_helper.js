e150f42b97ae6dd5edb971b38ab3401329288a82
Mike Triana <mikejtriana@gmail.com>
2020-11-18 16:12:07 -0500

    Remove unnecessary async label from getToken

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -2,7 +2,7 @@ const config = require('../utils/config')
 const jwt = require('jsonwebtoken')
 
 // Get a valid token for a specific user
-const getToken = async (user) => {
+const getToken = (user) => {
     return jwt.sign(user.toJSON(), config.PRIVATE_KEY, {
         expiresIn: 86400 //1 day
     })
