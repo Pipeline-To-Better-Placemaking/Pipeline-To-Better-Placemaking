e58606112c73af52a82a65dea2af514f3cde3b17
Mike Triana <mikejtriana@gmail.com>
2021-01-12 21:00:36 -0500

    Add additional test cases

--- backend/tests/new_user.test.js
+++ backend/tests/new_user.test.js
@@ -24,7 +24,7 @@ describe('When creating a new user account', () => {
             password: '!1Abcdef'
         }
 
-        const response = await api
+        await api
             .post(baseUrl)
             .send(newUser)
             // Confirm success
@@ -45,6 +45,269 @@ describe('When creating a new user account', () => {
                 })
             ])
         )
+    })
+
+    test('creation fails (400) with missing email', async () => {
+        const usersBefore = await th.getUsers()
+        
+        const newUser = {
+            firstname: 'John',
+            lastname: 'Doe',
+            password: '!1Abcdef'
+        }
+
+        await api
+            .post(baseUrl)
+            .send(newUser)
+            // Confirm failure
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAfter = await th.getUsers()
+        // Check if there is still the same numer of users as before
+        expect(usersAfter).toHaveLength(usersBefore.length)
+
+        // Check to make sure this user did not get added to the database
+        expect(usersAfter).not.toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname
+                })
+            ])
+        )
+    })
+
+    test('creation fails (400) with missing password', async () => {
+        const usersBefore = await th.getUsers()
+        
+        const newUser = {
+            firstname: 'John',
+            lastname: 'Doe',
+            email: 'test@yahoo.com'
+        }
+
+        await api
+            .post(baseUrl)
+            .send(newUser)
+            // Confirm failure
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAfter = await th.getUsers()
+        // Check if there is still the same numer of users as before
+        expect(usersAfter).toHaveLength(usersBefore.length)
+
+        // Check to make sure this user did not get added to the database
+        expect(usersAfter).not.toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    email: newUser.email
+                })
+            ])
+        )
+    })
+
+    test('creation fails (400) with invalid email', async () => {
+        const usersBefore = await th.getUsers()
+        
+        const newUser = {
+            firstname: 'John',
+            lastname: 'Doe',
+            email: 'invalidemail.com',
+            password: '!1Abcdef'
+        }
+
+        await api
+            .post(baseUrl)
+            .send(newUser)
+            // Confirm failure
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAfter = await th.getUsers()
+        // Check if there is still the same numer of users as before
+        expect(usersAfter).toHaveLength(usersBefore.length)
+
+        // Check to make sure this user did not get added to the database
+        expect(usersAfter).not.toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    firstname: newUser.firstname,
+                    lastname: newUser.lastname,
+                    email: newUser.email
+                })
+            ])
+        )
+    })
+
+    describe('creation fails (400) with invalid password', () => {
+        test('too short', async () => {
+            const usersBefore = await th.getUsers()
+            
+            const newUser = {
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'test@yahoo.com',
+                password: '!1Abcde'
+            }
+    
+            await api
+                .post(baseUrl)
+                .send(newUser)
+                // Confirm failure
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+    
+            const usersAfter = await th.getUsers()
+            // Check if there is still the same numer of users as before
+            expect(usersAfter).toHaveLength(usersBefore.length)
+    
+            // Check to make sure this user did not get added to the database
+            expect(usersAfter).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email
+                    })
+                ])
+            )
+        })
+
+        test('contains whitespace', async () => {
+            const usersBefore = await th.getUsers()
+            
+            const newUser = {
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'test@yahoo.com',
+                password: '!1Ab def'
+            }
+    
+            await api
+                .post(baseUrl)
+                .send(newUser)
+                // Confirm failure
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+    
+            const usersAfter = await th.getUsers()
+            // Check if there is still the same numer of users as before
+            expect(usersAfter).toHaveLength(usersBefore.length)
+    
+            // Check to make sure this user did not get added to the database
+            expect(usersAfter).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email
+                    })
+                ])
+            )
+        })
+
+        test('does not have a number', async () => {
+            const usersBefore = await th.getUsers()
+            
+            const newUser = {
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'test@yahoo.com',
+                password: '!aAbcdef'
+            }
+    
+            await api
+                .post(baseUrl)
+                .send(newUser)
+                // Confirm failure
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+    
+            const usersAfter = await th.getUsers()
+            // Check if there is still the same numer of users as before
+            expect(usersAfter).toHaveLength(usersBefore.length)
+    
+            // Check to make sure this user did not get added to the database
+            expect(usersAfter).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email
+                    })
+                ])
+            )
+        })
+
+        test('does not have a symbol', async () => {
+            const usersBefore = await th.getUsers()
+            
+            const newUser = {
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'test@yahoo.com',
+                password: 'a1Abcdef'
+            }
+    
+            await api
+                .post(baseUrl)
+                .send(newUser)
+                // Confirm failure
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+    
+            const usersAfter = await th.getUsers()
+            // Check if there is still the same numer of users as before
+            expect(usersAfter).toHaveLength(usersBefore.length)
+    
+            // Check to make sure this user did not get added to the database
+            expect(usersAfter).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email
+                    })
+                ])
+            )
+        })
+
+        test('does not have a capital letter', async () => {
+            const usersBefore = await th.getUsers()
+            
+            const newUser = {
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'test@yahoo.com',
+                password: '!1abcdef'
+            }
+    
+            await api
+                .post(baseUrl)
+                .send(newUser)
+                // Confirm failure
+                .expect(400)
+                .expect('Content-Type', /application\/json/)
+    
+            const usersAfter = await th.getUsers()
+            // Check if there is still the same numer of users as before
+            expect(usersAfter).toHaveLength(usersBefore.length)
+    
+            // Check to make sure this user did not get added to the database
+            expect(usersAfter).not.toEqual(
+                expect.arrayContaining([
+                    expect.objectContaining({
+                        firstname: newUser.firstname,
+                        lastname: newUser.lastname,
+                        email: newUser.email
+                    })
+                ])
+            )
+        })
     })
 })
 
