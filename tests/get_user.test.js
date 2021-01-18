b93ef3608fd4e097b27ab3c89b92838541a03cf2
Mike Triana <mikejtriana@gmail.com>
2021-01-18 18:09:11 -0500

    Add test cases for users/get

---
+++ tests/get_user.test.js
@@ -0,0 +1,96 @@
+const app = require('../app')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const api = supertest(app)
+const th = require('./test_helper')
+const User = require('../models/users')
+
+const baseUrl = '/api/users'
+
+const testUser = {
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'test@yahoo.com',
+    password: '1#Aadmin'
+}
+
+describe('When getting user info', () => {
+
+    let id = null
+    let token = null
+
+    beforeAll(async () => {
+        // Begin with a known user record in the database
+
+        await User.deleteMany({})
+        const user = new User({
+            firstname: testUser.firstname,
+            lastname: testUser.lastname,
+            email: testUser.email,
+            password: testUser.password
+        })
+        const savedUser = await User.addUser(user)
+        id = savedUser._id
+        token = th.getToken(user)
+    })
+
+    test('all info is gotten when user\'s token is given', async () => {
+        const response = await api
+            .get(`${baseUrl}/${id}`)
+            .set('Authorization', 'Bearer ' + token)
+            // Confirm successful 
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        // Confirm that response contains user's info
+        expect(response.body).toMatchObject({
+            id: id,
+            firstname: testUser.firstname,
+            lastname: testUser.lastname,
+            email: testUser.email
+        })
+        expect(response.body.isVerified).toBeDefined()
+        expect(response.body.invites).toBeDefined()
+        expect(response.body.teams).toBeDefined()
+
+        // Password should not be included
+        expect(response.body.password).not.toBeDefined()
+    })
+
+    test('only public info is gotten when no token is given', async () => {
+        const response = await api
+            .get(`${baseUrl}/${id}`)
+            // Confirm successful 
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        // Confirm that response contains user's info
+        expect(response.body).toMatchObject({
+            id: id,
+            firstname: testUser.firstname,
+            lastname: testUser.lastname,
+            email: testUser.email
+        })
+        expect(response.body.teams).toBeDefined()
+
+        // Private info should not be included
+        expect(response.body.isVerified).not.toBeDefined()
+        expect(response.body.invites).not.toBeDefined()
+        // Password should not be included
+        expect(response.body.password).not.toBeDefined()
+    })
+
+    test('request fails (404) if user does not exist', async () => {
+        await api
+            .get(`${baseUrl}/1234`)
+            // Confirm failure 
+            .expect(404)
+            .expect('Content-Type', /application\/json/)
+    })
+
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+    app.close()
+})
