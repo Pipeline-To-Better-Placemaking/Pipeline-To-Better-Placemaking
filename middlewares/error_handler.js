5c6b6d9c9e46424392c6dd4b189b6c659c57c030
Mike Triana <mikejtriana@gmail.com>
2021-02-03 19:05:45 -0500

    Update error handler and implement custom error classes

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,14 +1,35 @@
 const log = require('../utils/log')
+const { GeneralError } = require('../utils/errors')
 
 module.exports = (error, req, res, next) => {
     log.error(error.message)
 
-    if (error.name === 'ValidationError') {
-        return res.status(400).json({
+    // Manually thrown errors
+    if (error instanceof GeneralError) {
+        return res.status(error.getStatusCode()).json({
             success: false,
-            msg: error.message
+            message: error.message
         })
     }
 
-    next(error)
+    // Errors thrown by mongoose validation
+    if (error.name === 'ValidationError') {
+        const errorList = Object.keys(error.errors).map(k => {
+            if (error.errors[k].kind === 'unique') {
+                return `Duplicate field: ${error.errors[k].path}`
+            }
+            return `Missing or invalid field: ${error.errors[k].path}`
+        }).join(', ')
+
+        return res.status(400).json({
+            success: false,
+            message: errorList
+        })
+    }
+
+    // Other errors
+    return res.status(500).json({
+        success: false,
+        message: 'The server encountered a problem'
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 const User = require('../models/users.js')
+const { BadRequestError } = require('../utils/errors.js')
 
 // Create a new user
 router.post('/', async (req, res, next) => {
@@ -12,10 +13,7 @@ router.post('/', async (req, res, next) => {
         !/\d/g.test(req.body.password) ||           // Must contain at least one digit
         !/[!@#$%^&*]/g.test(req.body.password) ||   // Must contain at least one symbol
         !/[A-Z]/g.test(req.body.password)) {        // Must contain at least one uppercase letter
-        return res.status(400).json({
-            success: false,
-            msg: 'Missing or invalid password'
-        })
+        throw new BadRequestError('Missing or invalid field: password')
     }
 
     let newUser = new User({
