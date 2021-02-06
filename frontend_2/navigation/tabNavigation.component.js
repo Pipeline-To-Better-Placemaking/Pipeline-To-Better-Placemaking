e6a88129e971ca3c931804f3a5ecac68fa68a41c
madisonDishman <dishman.madison@knights.ucf.edu>
2021-02-06 12:44:09 -0500

    Loaded User Info (mostly a copy paste)

--- frontend_2/navigation/tabNavigation.component.js
+++ frontend_2/navigation/tabNavigation.component.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BottomNavigation, BottomNavigationTab, Layout, Text, Icon } from '@ui-kitten/components';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { HomeScreenStack } from './homeStack.component';
 import { UserSettingsStack } from './userStack.component';
 import { CollaborateStack } from './collaborateStack.component';
 
 const { Navigator, Screen } = createBottomTabNavigator();
+var teams;
+var invites;
+var userDetails = {
+    firstname: '',
+    lastname: '',
+    email: ''
+};
 
 const PersonIcon = (props) => (
     <Icon {...props} name='person-outline'/>
@@ -31,25 +39,66 @@ const BottomTabBar = ({ navigation, state }) => (
   </BottomNavigation>
 );
 
-export const TabNavigation = () => (
-  <Navigator
-    initialRouteName="HomeScreenStack"
-    tabBar={props => <BottomTabBar {...props} />}
-  >
-    <Screen
-      name="CollaborateStack"
-      component={CollaborateStack}
+export const TabNavigation = () => {
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      let token = await AsyncStorage.getItem("@token")
+      let id = await AsyncStorage.getItem("@id")
+      let success = false
+
+      await fetch('https://measuringplacesd.herokuapp.com/api/users/' + id, {
+          method: 'GET',
+          headers: {
+              Accept: 'application/json',
+                  'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token
+          }
+      })
+      .then((response) => (response.json()))
+      .then(async (res) => (
+              console.log(res),
+
+              await AsyncStorage.setItem("@firstName", res.firstname),
+              await AsyncStorage.setItem("@lastName", res.lastname),
+              await AsyncStorage.setItem("@email", res.email),
+              await AsyncStorage.setItem("@teams", JSON.stringify(res.teams)),
+              await AsyncStorage.setItem("@invites", JSON.stringify(res.invites)),
+
+              userDetails = {
+                  firstName: res.firstname,
+                  lastName: res.lastname,
+                  email: res.email
+              },
+              teams = res.teams,
+              invites = res.invites
+          ))
+      .catch((error) => (console.log(error), success = false))
+    }
+
+    fetchMyAPI()
+  }, [])
+
+  return (
+    <Navigator
+      initialRouteName="HomeScreenStack"
+      tabBar={props => <BottomTabBar {...props} />}
     >
-    </Screen>
-    <Screen
-      name="HomeScreenStack"
-      component={HomeScreenStack}
-    >
-    </Screen>
-    <Screen
-      name="UserSettingsStack"
-      component={UserSettingsStack}
-    >
-    </Screen>
-  </Navigator>
-);
+      <Screen
+        name="CollaborateStack"
+        component={CollaborateStack}
+      >
+      </Screen>
+      <Screen
+        name="HomeScreenStack"
+        component={HomeScreenStack}
+      >
+      </Screen>
+      <Screen
+        name="UserSettingsStack"
+        component={UserSettingsStack}
+      >
+      </Screen>
+    </Navigator>
+  );
+};
