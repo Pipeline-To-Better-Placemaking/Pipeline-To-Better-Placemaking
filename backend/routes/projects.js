55fa64bb1361c13e8785a8f9fe22b1b64ebcb0d0
JHowellUCF <josh.howell@knights.ucf.edu>
2021-02-22 14:44:50 -0500

    Updated Project Post method, updated project/areas add and delete and added project/standing_points add and delete. Removed update methods for each, could add back in if necessary

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -11,6 +11,7 @@ const config = require('../utils/config')
 const { models } = require('mongoose')
 
 const { BadRequestError, UnauthorizedError } = require('../utils/errors')
+const areas = require('../models/areas.js')
 
 router.post('', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
     user = await req.user
@@ -43,9 +44,8 @@ router.post('', passport.authenticate('jwt',{session:false}), async (req, res, n
             standingPoints: pointIds,
             team: req.body.team,
             surveyDuration: req.body.surveyDuration,
-            movingDuration: req.body.stationaryDuration,
-            stationaryDuration: req.body.surveyDuration
-        
+            movingDuration: req.body.movingDuration,
+            stationaryDuration: req.body.stationaryDuration
         })
 
         const project = await Project.addProject(newProject)
@@ -68,7 +68,10 @@ router.put('/:id', passport.authenticate('jwt',{session:false}), async (req, res
     let newProject = new Project({
         title: (req.body.title ? req.body.title : project.title),
         description: (req.body.description ? req.body.description : project.description),
-        area: (req.body.area ? req.body.area : project.area)
+        area: (req.body.area ? req.body.area : project.area),
+        surveyDuration: (req.body.surveyDuration ? req.body.surveyDuration : project.surveyDuration),
+        movingDuration: (req.body.movingDuration ? req.body.movingDuration : project.movingDuration),
+        stationaryDuration: (req.body.surveyDuration ? req.body.stationaryDuration: project.stationaryDuration) 
     })
 
     if (await Team.isAdmin(project.team,user._id)){
@@ -89,7 +92,7 @@ router.delete('/:id', passport.authenticate('jwt',{session:false}), async (req, 
     project = await Project.findById(req.params.id)
     if(await Team.isAdmin(project.team,user._id)){
         await Team.removeProject(project.team,project._id)
-        await Stationary_Map.projectCleanup(project._id)
+    
         res.json(await Project.deleteProject(project._id))
     }
     else{
@@ -101,54 +104,26 @@ router.delete('/:id', passport.authenticate('jwt',{session:false}), async (req, 
 router.post('/:id/areas', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
     user = await req.user
     project = await Project.findById(req.params.id)
+
+    let newArea = new Area({
+        points: req.body.points
+    })
+
+    newArea.save()
+
     if(await Team.isUser(project.team,user._id)){
         
-        if(req.body.area.length < 3)
+        if(req.body.points.length < 3)
             throw new BadRequestError('Areas require at least three points')
         
-        res.json(await Project.addArea(project._id,req.body.area))
+        res.json(await Project.addArea(project._id,newArea._id))
     }
     else{
         throw new UnauthorizedError('You do not have permision to perform this operation')
     }
 })
 
-router.put('/:id/areas/:areaId', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    user = await req.user   
-    project = await Project.findById(req.params.id)
-    if (await Team.isAdmin(project.team,user._id)){
-        res.status(201).json(await Project.updateArea(project._id,req.params.areaId,req.body.area))
-    }  
-    else{
-        throw new UnauthorizedError('You do not have permision to perform this operation')
-    }  
-})
-
-router.put('/:id/areas/', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
-    project = await Project.findById(req.params.id)
-    if (await Team.isAdmin(project.team,user._id)){
-
-        if(req.body.areas){
-            
-            for(var i = 0; i < req.body.areas.length; i++){
-                if(req.body.areas[i].length < 3)
-                throw new BadRequestError('Area ' + i + ' requires at least three points.')
-            }
-            for(var i = 0; i < req.body.areas.length; i++){
-                await Project.updateArea(project._id,req.body.areas[i].id,req.body.areas[i].points)
-            }
-            res.status(201).json(await Project.findById(req.params.id))
-        }
-        else{
-            await Project.updateArea(project._id,area.id,req.body.area)
-            res.status(201).json(await Project.findById(req.params.id))
-        }   
-    }
-    else{
-        throw new UnauthorizedError('You do not have permision to perform this operation')
-    }
-})
-router.delete('/:id/areas/:areadId', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+router.delete('/:id/areas/:areaId', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
     user = await req.user
     project = await Project.findById(req.params.id)
     if(await Team.isAdmin(project.team,user._id)){
@@ -159,5 +134,34 @@ router.delete('/:id/areas/:areadId', passport.authenticate('jwt',{session:false}
     }
 })
 
+router.post('/:id/standing_points', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+    user = await req.user
+    project = await Project.findById(req.params.id)
+
+    let newPoint = new Standing_Point({
+        longitude: req.body.longitude,
+        latitude: req.body.latitude,
+        title: req.body.title
+    })
+    newPoint.save()
+
+    if(await Team.isUser(project.team,user._id)){      
+        res.json(await Project.addPoint(project._id,newPoint._id))
+    }
+    else{
+        throw new UnauthorizedError('You do not have permision to perform this operation')
+    }
+})
+
+router.delete('/:id/standing_points/:pointId', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+    user = await req.user
+    project = await Project.findById(req.params.id)
+    if(await Team.isAdmin(project.team,user._id)){
+        res.status(201).json(await Project.deleteArea(project._id,req.params.pointId))
+    }
+    else{
+        throw new UnauthorizedError('You do not have permision to perform this operation')
+    }
+})
 
 module.exports = router
