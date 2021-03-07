e6a40afb0fe87c7aae4604216e14224e203721fa
JHowellUCF <josh.howell@knights.ucf.edu>
2021-03-07 18:54:35 -0500

    added a api/collections route where you can get collections/stationary/:id to get info about your collection

---
+++ backend/routes/collections.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const Project = require('../models/projects.js')
+const Stationary_Collection = require('../models/stationary_collections.js')
+const Moving_Collection = require('../models/moving_collections.js')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+const { models } = require('mongoose')
+
+const { BadRequestError, UnauthorizedError } = require('../utils/errors')
+
+
+router.get('/moving/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+
+    res.status(200).json(await Moving_Collection.findById(req.param.id)
+                                                .populate('area')
+                                                .populate('maps'))
+
+})
+
+router.get('/stationary/:id', passport.authenticate('jwt',{session:false}), async (req, res, next) => {
+
+    res.status(200).json(await Stationary_Collection.findById(req.param.id)
+                                                .populate('area')
+                                                .populate('maps'))
+
+})
+
+module.exports = router
