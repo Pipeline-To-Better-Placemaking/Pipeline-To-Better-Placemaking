24022ba77caaa95877ab666250d3d3770a5638b1
Mike Triana <mikejtriana@gmail.com>
2021-03-09 20:29:48 -0500

    Add email verification tab to user settings

--- frontend_2/screens/UserSettings/userSettings.component.js
+++ frontend_2/screens/UserSettings/userSettings.component.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Modal, ScrollView } from 'react-native';
 import { Icon, Input, Text, Button } from '@ui-kitten/components';
 import { ThemeContext } from '../../theme-context';
@@ -10,10 +10,22 @@ import { PropsService } from '@ui-kitten/components/devsupport';
 
 export function UserSettings(props) {
 
-  const [modalVisible, setModalVisible] = useState(false);
+  const [settingsModalVisible, setSettingsModalVisible] = useState(false);
+  const [verifyModalVisible, setVerifyModalVisible] = useState(false);
   const [firstNameText, setFirstNameText] = useState(props.firstName);
   const [lastNameText, setLastNameText] = useState(props.lastName);
   const [emailText, setEmailText] = useState(props.lastName);
+  const [isVerified, setIsVerified] = useState(false);
+  const [verificationCode, setVerificationCode] = useState('');
+  const [verificationError, setVerificationError] = useState(false);
+  const [codeChanged, setCodeChanged] = useState(false);
+
+  useEffect(() => {
+      async function fetchData() {
+          setIsVerified(await AsyncStorage.getItem('@isVerified') === 'true')
+      }
+      fetchData()
+  })
 
   const themeContext = React.useContext(ThemeContext);
 
@@ -53,7 +65,7 @@ export function UserSettings(props) {
     setFirstNameText(props.firstName)
     setLastNameText(props.lastName)
     setEmailText(props.email)
-    setModalVisible(!modalVisible)
+    setSettingsModalVisible(!settingsModalVisible)
   }
 
   const confirmEditProfile = async () => {
@@ -110,7 +122,7 @@ export function UserSettings(props) {
       //props.setEmail(emailText)
       //await AsyncStorage.setItem('@email', props.email)
 
-      setModalVisible(!modalVisible)
+      setSettingsModalVisible(!settingsModalVisible)
     } else {
       cancelEditProfile()
     }
@@ -125,12 +137,71 @@ export function UserSettings(props) {
     await props.navigation.navigate('Title');
   }
 
+  const verifyEmail = async () => {
+        const email = await AsyncStorage.getItem('@email');
+
+        let success = false;
+        let result = null;
+
+        try {
+            const response = await fetch(`https://measuringplacesd.herokuapp.com/api/verify/?email=${email}&code=${verificationCode}`, {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            result = await response.text();
+            console.log(result);
+            success = response.status === 200;
+
+        } catch (error) {
+            console.log('ERROR: ' + error);
+        }
+
+        if(success) {
+            setIsVerified(true);
+            await AsyncStorage.setItem('@isVerified', true);
+            setVerifyModalVisible(false);
+        } else {
+            setVerificationError(true);
+        }
+  }
+
+  const resendCode = async () => {
+        let success = false;
+        let result = null;
+
+        try {
+            const response = await fetch('https://measuringplacesd.herokuapp.com/api/verify/newcode', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + props.token
+                }
+            });
+
+            result = await response.text();
+            console.log(result);
+            success = response.status === 200;
+
+        } catch (error) {
+            console.log('ERROR: ' + error);
+        }
+
+        if (success) {
+            setCodeChanged(true);
+        }
+  }
+
   return (
     <ViewableArea>
       <Header text={'User Settings'}/>
 
       <ContentContainer>
-        <Modal animationType="slide" visible={modalVisible}  onRequestClose={() => {setModalVisible(!modalVisible)}}>
+        <Modal animationType="slide" visible={settingsModalVisible}  onRequestClose={() => {setSettingsModalVisible(!settingsModalVisible)}}>
           <ViewableArea>
             <Header text={'Edit Profile'}/>
             <ContentContainer>
@@ -182,6 +253,65 @@ export function UserSettings(props) {
           </ViewableArea>
         </Modal>
 
+        <Modal animationType="slide" visible={verifyModalVisible}  onRequestClose={() => {setVerifyModalVisible(!verifyModalVisible)}}>
+            <ViewableArea>
+                <Header text='Verify Email'/>
+                <ContentContainer>
+                    <View style={{margin:20}}>
+                        <Text category='s1'>Enter verification code:</Text>
+                        <Input
+                            placeholder = 'Code'
+                            value={verificationCode}
+                            onChangeText={nextValue => setVerificationCode(nextValue)}
+                            caption={
+                                verificationError &&
+                                <Text style={{color: '#FF3D71'}}>
+                                    Failed to verify email. Code may be incorrect or expired.
+                                </Text>
+                            }
+                        />
+
+                        <Button
+                            status={'success'}
+                            style={{margin:5}}
+                            onPress={() => verifyEmail()}
+                            accessoryRight = {confirmIcon}
+                        >
+                            SUBMIT
+                        </Button>
+
+                        <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                            <Button
+                                status={'danger'}
+                                style={{margin:5}}
+                                onPress={() => {
+                                    setVerificationCode('')
+                                    setVerificationError(false)
+                                    setVerifyModalVisible(!verifyModalVisible)
+                                }}
+                                accessoryLeft = {closeIcon}
+                            >
+                                CANCEL
+                            </Button>
+                            <Button
+                                style={{margin:5}}
+                                onPress={() => resendCode()}
+                            >
+                                RESEND CODE
+                            </Button>
+                        </View>
+
+                        {
+                            codeChanged &&
+                            <Text>
+                                Sent a new verification code to your email. Please check your inbox and spam folder.
+                            </Text>
+                        }
+                    </View>
+                </ContentContainer>
+            </ViewableArea>
+        </Modal>
+
         <ScrollView style={{margin:5, marginRight:20, marginLeft:20}}>
 
           <View style={styles.circle}>
@@ -195,7 +325,7 @@ export function UserSettings(props) {
             <Text style={{fontSize: 20, alignSelf: 'center'}}> {props.email} </Text>
           </View>
 
-          <Button style={{margin:5}} onPress={() => setModalVisible(!modalVisible)} accessoryRight = {editIcon}>
+          <Button style={{margin:5}} onPress={() => setSettingsModalVisible(!settingsModalVisible)} accessoryRight = {editIcon}>
             EDIT PROFILE
           </Button>
 
@@ -206,6 +336,13 @@ export function UserSettings(props) {
           <Button style={{margin:5}}>
             CHANGE PASSWORD
           </Button>
+
+          {
+              !isVerified &&
+              <Button style={{margin:5}} onPress={() => setVerifyModalVisible(!verifyModalVisible)}>
+                VERIFY EMAIL
+              </Button>
+          }
 
           <Button style={{margin:5}} status='danger' onPress={() => logOut()}>
             LOG OUT
