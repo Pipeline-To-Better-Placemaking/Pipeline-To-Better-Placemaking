f8aa1798df7ba19b37a29c70c773a1cce64c33be
JHowellUCF <josh.howell@knights.ucf.edu>
2021-03-21 21:20:21 -0400

    added sureys & survey collection

---
+++ models/survey_collections.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const Date = mongoose.Schema.Types.Date
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const Surveys = require('../models/surveys.js')
+
+
+const collection_schema = mongoose.Schema({
+    title: String,
+    date: {
+        type: Date,
+        required: true
+    },
+
+    duration: {
+        type: Number,
+        required: true,
+        default: 15
+    },
+
+    surveys: [{
+        type: ObjectId,
+        ref: 'Surveys'
+    }]
+
+})
+
+const Collection = module.exports = mongoose.model('Survey_Collections', collection_schema)
+
+module.exports.deleteMap = async function(collectionId, surveyId){
+    await Collection.findByIdAndDelete(surveyId)
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $pull: { surveys: surveyId}}
+    )
+
+}
+
+module.exports.deleteCollection = async function(collectionId){
+    collection = await Collection.findById(collectionId)
+
+    for(var i = 0; i < collection.surveys.length; i++)
+        await Survey.findByIdAndDelete(collection.surveys[i])
+
+    return await Collection.findByIdAndDelete(collectionId)
+}
+
+module.exports.addActivity = async function(collectionId, surveyId){
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $push: { surveys: surveyId}}
+    )
+}
+
+module.exports.updateCollection = async function(collectionId, newCollection){
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $set: {
+            title: newCollection.title,
+            time: newCollection.date,
+            duration: newCollection.duration
+        }}
+    )
+}
