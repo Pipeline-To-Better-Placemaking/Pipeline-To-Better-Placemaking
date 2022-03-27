cff8a32648b82d18f23c09a113c5471a1f3e7087
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-03-27 09:53:45 -0400

    Edited to include Mongoose functions

--- backend/models/sound_maps.js
+++ backend/models/sound_maps.js
@@ -85,3 +85,120 @@ const stationary_schema = mongoose.Schema({
 
 const Maps = module.exports = mongoose.model('Sound_Maps', stationary_schema)
 const Entry = mongoose.model('Data_Entry', dataSchema)
+
+module.exports.addMap = async function(newMap) {
+    return await newMap.save()
+}
+
+module.exports.updateMap = async function (projectId, newMap) {
+    return await Maps.updateOne(
+        { _id: projectId },
+        { $set: {
+            title: newMap.title,
+            date: newMap.date,
+            maxResearchers: newMap.maxResearchers,
+            standingPoints: newMap.standingPoints
+        }}
+    )
+}
+
+module.exports.deleteMap = async function(mapId) {
+
+    const map = await Maps.findById(mapId)
+
+    for(var i = 0; i < map.standingPoints.length; i++)
+        Points.removeRefrence(map.standingPoints[i])
+    
+    return await Maps.findByIdAndDelete(mapId)
+}
+
+module.exports.projectCleanup = async function(projectId) {
+    return await Maps.deleteMany({ project: projectId })
+}
+
+module.exports.addEntry = async function(mapId, newEntry) {
+    var entry = new Entry({
+        time: newEntry.time,
+        gender: newEntry.gender,
+        posture: newEntry.posture,
+        age: newEntry.age,
+        activity: newEntry.activity,
+        location: newEntry.location,
+        standingPoint: newEntry.standingPoint
+    })
+
+    Points.addRefrence(newEntry.standingPoints)
+
+    return await Maps.updateOne(
+        { _id: mapId },
+        { $push: { data: entry}}
+    )
+}
+
+module.exports.addResearcher = async function(mapId, userId){
+    return await Maps.updateOne(
+        { _id: mapId },
+        { $push: { researchers: userId}}
+    )
+}
+
+module.exports.removeResearcher = async function(mapId, userId){
+    return await Maps.updateOne(
+        { _id: mapId },
+        { $pull: { researchers: userId}}
+    )
+}
+
+module.exports.isResearcher = async function(mapId, userId){
+    try{
+        const doc = await Maps.find(
+            {
+                _id: mapId, 
+                researchers: { $elemMatch:  userId }
+            }
+        )
+    }catch(error){
+        return false
+    }
+    if (doc.length === 0) {
+        return false
+    }
+    return true
+}
+    
+module.exports.findData = async function(mapId, entryId){
+    const out = (await Maps.find({
+        _id: mapId,
+        'data._id': entryId 
+    },
+    {'data.$':1}))
+
+    return out[0].data[0]
+}
+
+module.exports.updateData = async function(mapId, dataId, newEntry){
+    return await Maps.updateOne(
+        {
+            _id: mapId,
+            'data._id': dataId 
+        },
+        { $set: { "data.$": newEntry}}
+    )}
+
+module.exports.deleteEntry = async function(mapId, entryId) {
+    
+        const doc = await Maps.find(
+            {   
+                _id: mapId, 
+                data: { $elemMatch:  {_id:entryId }}
+            }
+        )
+    
+        await Points.removeRefrence(doc.data[0].standingPoint)
+    
+        return await Maps.updateOne(
+            { _id: mapId },
+            { $pull: { data: {_id:entryId }}
+            })
+    }
+    
