efad87cb671fc9b10e3dca24eaeadf3303f40c4d
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-03-30 09:55:49 -0400

    Update nature_collections.js

--- backend/models/nature_collections.js
+++ backend/models/nature_collections.js
@@ -32,3 +32,40 @@ const collection_schema = mongoose.Schema({
 })
 
 const Collection = module.exports = mongoose.model('Nature_Collections', collection_schema)
+
+module.exports.deleteMap = async function(collectionId, mapId){
+    await Nature_Maps.deleteMap(mapId)
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $pull: { maps: mapId}}
+    )
+
+}
+
+module.exports.deleteCollection = async function(collectionId){
+    collection = await Collection.findById(collectionId)
+    await Area.removeRefrence(collection.area)
+    for(var i = 0; i < collection.maps.length; i++)
+        await Nature_Maps.findByIdAndDelete(collection.maps[i])
+
+    return await Collection.findByIdAndDelete(collectionId)
+}
+
+module.exports.addActivity = async function(collectionId, mapId){
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $push: { maps: mapId}}
+    )
+}
+
+module.exports.updateCollection = async function(collectionId, newCollection){
+    return await Collection.updateOne(
+        { _id: collectionId },
+        { $set: {
+            title: newCollection.title,
+            time: newCollection.date,
+            area: newCollection.area,
+            duration: newCollection.duration
+        }}
+    )
+}
