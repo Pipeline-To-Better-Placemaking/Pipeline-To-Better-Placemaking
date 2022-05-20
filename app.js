0847aa825090883ef6c5cb75dd4f16916d06b2dd
Ron Campos <roncamposj@gmail.com>
2022-05-20 16:38:25 -0400

    added app.get with resolved path as 'public'

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ const app = express();
 app.use(cors())
 
 app.use(express.static(path.join(__dirname,'public')))
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
+
 
 app.use(bodyParser.json())
 
