0cc22828319a5dd6ff5a3f11944f19a2da7bb613
Ron Campos <roncamposj@gmail.com>
2022-05-20 17:40:05 -0400

    added build to app.get

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors())
 
 app.use(express.static(path.join(__dirname,'public')))
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build/frontend_web/public", "index.html")))
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build")))
 
 
 app.use(bodyParser.json())
