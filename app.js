f612063b547f7947951481d746bb943413df13b7
Ron Campos <roncamposj@gmail.com>
2022-05-20 16:46:17 -0400

    added app.get with resolved path as 'frontend_web/public'

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 app.use(cors())
 
 app.use(express.static(path.join(__dirname,'public')))
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "public", "index.html")));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "frontend_web/public", "index.html")));
 
 
 app.use(bodyParser.json())
