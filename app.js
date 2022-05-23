dacc9d5e54191712341eb682802fdb826187caf7
Ron Campos <roncamposj@gmail.com>
2022-05-23 09:12:55 -0400

    adding build to app.use before calling app.get

--- app.js
+++ app.js
@@ -28,8 +28,10 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname,'public')))
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+app.use(express.static('build'))
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'))
   })
 
 app.use(bodyParser.json())
