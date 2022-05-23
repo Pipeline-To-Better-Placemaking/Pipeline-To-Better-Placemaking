77bb20050bc3e7bc597a2ec10a3f423f369552ef
Ron Campos <roncamposj@gmail.com>
2022-05-23 12:53:23 -0400

    added headers

--- app.js
+++ app.js
@@ -25,7 +25,24 @@ connect()
 const app = express();
 
 app.use(cors())
+app.use((req, res, next) => {
 
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    
+    next();
+
+});
 app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', function (req, res) {
