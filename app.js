22aae09665661b8b84b09d88b782840e455c6fee
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-24 11:03:15 -0400

    added /* now to app.get (still in final lines of code)

--- app.js
+++ app.js
@@ -75,9 +75,10 @@ app.use(errorHandler)
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 const server = app.listen(config.PORT, () => {
     log.info(`Server is running on port ${config.PORT}`)
 })
