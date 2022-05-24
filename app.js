6d66f6d9766bf15de66e82eb1af2cbd2810855fc
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-24 11:10:59 -0400

    changed directory paths

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use(errorHandler)
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontent_web','public', 'index.html'));
 });
 
 const server = app.listen(config.PORT, () => {
