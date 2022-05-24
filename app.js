142825b32063c4a541801b899f3d902b8e3d8b3b
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-24 18:30:16 -0400

    readded manifest.json with edit to url and changed app.get path and homepage to package.json in frontend and removed app.use public

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ app.use(errorHandler)
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const server = app.listen(config.PORT, () => {
     log.info(`Server is running on port ${config.PORT}`)
