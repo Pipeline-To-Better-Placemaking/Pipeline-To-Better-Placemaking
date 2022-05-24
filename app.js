301e7606eb6e23c1a796cfd9590006fdc8c8869e
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-23 20:05:04 -0400

    added app.use() with build below app.use() public

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'build')))
 
 app.get('/', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'frontend_web','public', 'index.html'))
