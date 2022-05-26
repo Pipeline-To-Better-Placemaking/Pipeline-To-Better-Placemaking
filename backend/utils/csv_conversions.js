1cbe7c385fd48a80cdca1aea179a31c0664f3a3e
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-05-25 20:01:38 -0400

    Updates reflect current data entries in schemas

--- backend/utils/csv_conversions.js
+++ backend/utils/csv_conversions.js
@@ -120,6 +120,307 @@ movingToCSV = function(data) {
     return csv
 }
 
+soundToCSV = function(data) {
+
+    var headers = "Collection_Title," +
+                  "Collection_Date,"  +
+                  "Area_Title,Area,Duration,Activity_Time," +
+                  "Researchers,Standing_Point,Standing_Point_Title," +
+                  "Sound_Type,Decibal_1,Decibal_2,Decibal_3,Decibal_4,Decibal_5,Average"
+
+    var csv = headers
+
+    for(var i = 0; i < data.movingCollections.length; i++){
+        var collection = data.movingCollections[i]
+        
+        var area = "\"POLYGON (("
+        for(var j = 0; j < collection.area.points.length; j++){
+            if (j != 0) area += ','
+            area += collection.area.points[j].latitude + " "
+            area += collection.area.points[j].longitude
+        }
+        area += "))\""
+        
+        if(collection.maps){
+            for (var j = 0; j < collection.maps.length; j++){
+                var map = collection.maps[j]
+                var researchers = "\""
+                if(map.researchers){
+                    for(var k = 0; k < map.researchers.length; k++){
+                        if(k != 0) researchers += ', '
+                        researchers += map.researchers[k].firstname + " " + map.researchers[k].lastname
+                    }
+                }
+                researchers += "\""
+                if(map.data){
+                    for(var k = 0; k < map.data.length; k++){
+                        var entry = map.data[k]
+                        csv += '\n'
+                        csv += collection.title + ','
+                        csv += collection.date + ','
+                        csv += collection.area.title + ','
+                        csv += area + ','
+                        csv += collection.duration + ','
+                        csv += map.date + ','
+                        csv += researchers + ','
+                        csv += entry.standingPoint.title + ','
+                        csv += "\"POINT( " + entry.standingPoint.latitude + " " + entry.standingPoint.longitude + ")\","
+                        csv += entry.sound_type + ','
+                        csv += entry.decibal_1 + ','
+                        csv += entry.decibal_2 + ','
+                        csv += entry.decibal_3 + ','
+                        csv += entry.decibal_4 + ','
+                        csv += entry.decibal_5 + ','
+                        csv += entry.average
+                    }
+                }
+            }
+        }
+    }
+
+    return csv
+}
+
+lightToCSV = function(data) {
+
+    var headers = "Collection_Title," +
+                  "Collection_Date,"  +
+                  "Area_Title,Area,Duration,Activity_Time," +
+                  "Researchers,Standing_Point,Standing_Point_Title," +
+                  "Light_Description,Path"
+
+    var csv = headers
+
+    for(var i = 0; i < data.movingCollections.length; i++){
+        var collection = data.movingCollections[i]
+        
+        var area = "\"POLYGON (("
+        for(var j = 0; j < collection.area.points.length; j++){
+            if (j != 0) area += ','
+            area += collection.area.points[j].latitude + " "
+            area += collection.area.points[j].longitude
+        }
+        area += "))\""
+        
+        if(collection.maps){
+            for (var j = 0; j < collection.maps.length; j++){
+                var map = collection.maps[j]
+                var researchers = "\""
+                if(map.researchers){
+                    for(var k = 0; k < map.researchers.length; k++){
+                        if(k != 0) researchers += ', '
+                        researchers += map.researchers[k].firstname + " " + map.researchers[k].lastname
+                    }
+                }
+                researchers += "\""
+                if(map.data){
+                    for(var k = 0; k < map.data.length; k++){
+                        var entry = map.data[k]
+                        csv += '\n'
+                        csv += collection.title + ','
+                        csv += collection.date + ','
+                        csv += collection.area.title + ','
+                        csv += area + ','
+                        csv += collection.duration + ','
+                        csv += map.date + ','
+                        csv += researchers + ','
+                        csv += entry.standingPoint.title + ','
+                        csv += "\"POINT( " + entry.standingPoint.latitude + " " + entry.standingPoint.longitude + ")\","
+                        csv += entry.light_description
+                        path = "\"LINESTRING ( "
+                        for(var l = 0; l < entry.path.length; l++){
+                           if (l != 0) path += ", "
+                           path += entry.path[l].latitude + " " + entry.path[l].longitude
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return csv
+}
+
+natureToCSV = function(data) {
+
+    var headers = "Collection_Title," +
+                  "Collection_Date,"  +
+                  "Area_Title,Area,Duration,Activity_Time," +
+                  "Researchers,Standing_Point,Standing_Point_Title," +
+                  "Location,Animals,Landscape,Weather,Water"
+
+    var csv = headers
+
+    for(var i = 0; i < data.movingCollections.length; i++){
+        var collection = data.movingCollections[i]
+        
+        var area = "\"POLYGON (("
+        for(var j = 0; j < collection.area.points.length; j++){
+            if (j != 0) area += ','
+            area += collection.area.points[j].latitude + " "
+            area += collection.area.points[j].longitude
+        }
+        area += "))\""
+        
+        if(collection.maps){
+            for (var j = 0; j < collection.maps.length; j++){
+                var map = collection.maps[j]
+                var researchers = "\""
+                if(map.researchers){
+                    for(var k = 0; k < map.researchers.length; k++){
+                        if(k != 0) researchers += ', '
+                        researchers += map.researchers[k].firstname + " " + map.researchers[k].lastname
+                    }
+                }
+                researchers += "\""
+                if(map.data){
+                    for(var k = 0; k < map.data.length; k++){
+                        var entry = map.data[k]
+                        csv += '\n'
+                        csv += collection.title + ','
+                        csv += collection.date + ','
+                        csv += collection.area.title + ','
+                        csv += area + ','
+                        csv += collection.duration + ','
+                        csv += map.date + ','
+                        csv += researchers + ','
+                        csv += entry.standingPoint.title + ','
+                        csv += "\"POINT( " + entry.standingPoint.latitude + " " + entry.standingPoint.longitude + ")\","
+                        csv += "\"POINT( " + entry.location.latitude + " " + entry.location.longitude + ")\","
+                        csv += entry.animals + ','
+                        csv += entry.landscape + ','
+                        csv += entry.weather + ','
+                        csv += entry.water
+                    
+                    }
+                }
+            }
+        }
+    }
+
+    return csv
+}
+
+orderToCSV = function(data) {
+
+    var headers = "Collection_Title," +
+                  "Collection_Date,"  +
+                  "Area_Title,Area,Duration,Activity_Time," +
+                  "Researchers,Standing_Point,Standing_Point_Title," +
+                  "Location,Garbage_Can,Building_Condition,Area_Lighting,Description"
+
+    var csv = headers
+
+    for(var i = 0; i < data.movingCollections.length; i++){
+        var collection = data.movingCollections[i]
+        
+        var area = "\"POLYGON (("
+        for(var j = 0; j < collection.area.points.length; j++){
+            if (j != 0) area += ','
+            area += collection.area.points[j].latitude + " "
+            area += collection.area.points[j].longitude
+        }
+        area += "))\""
+        
+        if(collection.maps){
+            for (var j = 0; j < collection.maps.length; j++){
+                var map = collection.maps[j]
+                var researchers = "\""
+                if(map.researchers){
+                    for(var k = 0; k < map.researchers.length; k++){
+                        if(k != 0) researchers += ', '
+                        researchers += map.researchers[k].firstname + " " + map.researchers[k].lastname
+                    }
+                }
+                researchers += "\""
+                if(map.data){
+                    for(var k = 0; k < map.data.length; k++){
+                        var entry = map.data[k]
+                        csv += '\n'
+                        csv += collection.title + ','
+                        csv += collection.date + ','
+                        csv += collection.area.title + ','
+                        csv += area + ','
+                        csv += collection.duration + ','
+                        csv += map.date + ','
+                        csv += researchers + ','
+                        csv += entry.standingPoint.title + ','
+                        csv += "\"POINT( " + entry.standingPoint.latitude + " " + entry.standingPoint.longitude + ")\","
+                        csv += "\"POINT( " + entry.location.latitude + " " + entry.location.longitude + ")\","
+                        csv += entry.garbage_can + ','
+                        csv += entry.building_condition + ','
+                        csv += entry.area_lighting + ','
+                        csv += entry.description
+                    }
+                }
+            }
+        }
+    }
+
+    return csv
+}
+
+boundariesToCSV = function(data) {
+
+    var headers = "Collection_Title," +
+                  "Collection_Date,"  +
+                  "Area_Title,Area,Duration,Activity_Time," +
+                  "Researchers,Standing_Point,Standing_Point_Title," +
+                  "Horizontal,Vertical,Path"
+
+    var csv = headers
+
+    for(var i = 0; i < data.movingCollections.length; i++){
+        var collection = data.movingCollections[i]
+        
+        var area = "\"POLYGON (("
+        for(var j = 0; j < collection.area.points.length; j++){
+            if (j != 0) area += ','
+            area += collection.area.points[j].latitude + " "
+            area += collection.area.points[j].longitude
+        }
+        area += "))\""
+        
+        if(collection.maps){
+            for (var j = 0; j < collection.maps.length; j++){
+                var map = collection.maps[j]
+                var researchers = "\""
+                if(map.researchers){
+                    for(var k = 0; k < map.researchers.length; k++){
+                        if(k != 0) researchers += ', '
+                        researchers += map.researchers[k].firstname + " " + map.researchers[k].lastname
+                    }
+                }
+                researchers += "\""
+                if(map.data){
+                    for(var k = 0; k < map.data.length; k++){
+                        var entry = map.data[k]
+                        csv += '\n'
+                        csv += collection.title + ','
+                        csv += collection.date + ','
+                        csv += collection.area.title + ','
+                        csv += area + ','
+                        csv += collection.duration + ','
+                        csv += map.date + ','
+                        csv += researchers + ','
+                        csv += entry.standingPoint.title + ','
+                        csv += "\"POINT( " + entry.standingPoint.latitude + " " + entry.standingPoint.longitude + ")\","
+                        csv += "\"HORIZONTAL( " + entry.horizontal.height + " " + entry.horizontal.description + ")\","
+                        csv += "\"VERTICAL( " + entry.vertical.kind + " " + entry.vertical.description + ")\","
+                        path = "\"LINESTRING ( "
+                        for(var l = 0; l < entry.path.length; l++){
+                           if (l != 0) path += ", "
+                           path += entry.path[l].latitude + " " + entry.path[l].longitude
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return csv
+}
+
 surveyToCSV = function(data) {
 
     var headers = "Collection_Title," +
