6485559a7f35ab4e8a3fccb091f2ed0e497951fe
Whisel <informer444@gmail.com>
2022-06-20 19:45:36 -0400

    Passing fix for teamless accounts

--- frontend_web/src/routes/Home.js
+++ frontend_web/src/routes/Home.js
@@ -4,19 +4,9 @@ import DisplayCards from '../components/DisplayCards';
 import { Link } from 'react-router-dom';
 
 function Home(props){
-    let i = 0;
 
-    const token = {
-        teams: props.passToken.user?.teams ? props.passToken.user.teams : '605e57007a68230004c01a2d'
-    }
-
-    //example format hardcoded on template, restructure the data or pass teams info from DB into <DisplayCards teams={DBTeams}/>
-    const teams = [
-        {
-            _id: token.teams[0], 
-            title: token.teams[0].title
-        }
-    ]
+    //holds passed token from App.js
+    const teams = props.passToken.user?.teams
 
     return(
         <div id='userHome'>
