64a5bb26198846b2a83fcb6b12bad3dd4bd691ca
roncamposj <42298486+roncamposj@users.noreply.github.com>
2022-06-30 18:34:08 -0400

    commit google map key

--- utils/config.js
+++ utils/config.js
@@ -10,6 +10,7 @@ let CLIENT_ID = process.env.CLIENT_ID
 let CLIENT_SECRET = process.env.CLIENT_SECRET
 let REFRESH_TOKEN = process.env.REFRESH_TOKEN
 let ACCESS_TOKEN = process.env.ACCESS_TOKEN
+let GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY
 
 if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
 	DB_URI = process.env.TEST_DB_URI
@@ -24,5 +25,6 @@ module.exports = {
     CLIENT_ID,
     CLIENT_SECRET,
     REFRESH_TOKEN,
-    ACCESS_TOKEN
+    ACCESS_TOKEN,
+    GOOGLE_MAP_KEY
 }
