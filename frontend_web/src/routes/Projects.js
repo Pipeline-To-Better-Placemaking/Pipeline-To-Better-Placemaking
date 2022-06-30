3420ed863aedf2812c12d76be0e85aa20e125e58
Whisel <informer444@gmail.com>
2022-06-30 10:18:04 -0400

    missing forward slash

--- frontend_web/src/routes/Projects.js
+++ frontend_web/src/routes/Projects.js
@@ -17,7 +17,7 @@ function Projects(props){
         // There can be multiple projects
 
         try {
-            const response = await fetch('https://p2bp.herokuapp.com/api/teams'+ teams, {
+            const response = await fetch('https://p2bp.herokuapp.com/api/teams/'+ teams, {
                 method: 'GET',
                 headers: { 
                     Accept: 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + props.token },
@@ -38,7 +38,7 @@ function Projects(props){
         let projectId = teamInfo?.projects;
 
         try {
-            const response = await fetch('https://p2bp.herokuapp.com/api/projects' + projectId, {
+            const response = await fetch('https://p2bp.herokuapp.com/api/projects/' + projectId, {
                 method: 'GET',
                 headers: { 
                     Accept: 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + props.token },
