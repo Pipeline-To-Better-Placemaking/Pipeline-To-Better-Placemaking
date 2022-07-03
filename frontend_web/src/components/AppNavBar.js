f67a378072dca81f0fa1eecabcafbfc2c6fbb1d3
Jess <70274122+J-Compsci@users.noreply.github.com>
2022-07-03 19:55:29 -0400

    Small Change
    +fixed user key access in AppNavBar

--- frontend_web/src/components/AppNavBar.js
+++ frontend_web/src/components/AppNavBar.js
@@ -39,7 +39,7 @@ const AppNavBar = (props) => {
     const segment = location.pathname.split('/');
 
     console.log('AppNavBar');
-    console.log(location.state.user.user.user);
+    console.log(location.state.userToken.user);
     
     const userName = {
         fN: location.state?.userToken?.user.firstname ? location.state?.userToken?.user.firstname : 'Abc',
