ae737c3256e06dc9995389dacb9b088662cdbb85
Jess <70274122+J-Compsci@users.noreply.github.com>
2022-07-10 08:42:13 -0400

    Testing useEffect timing
    +testing conditionals for deeper results

--- frontend_web/src/routes/ProjectPage.js
+++ frontend_web/src/routes/ProjectPage.js
@@ -1080,7 +1080,7 @@ function ProjectPage(){
     }
     //loc state recieved from (project type) Display Cards on TeamHome(listing team projects)
     const loc = useLocation();
-    const [loaded, setLoaded] = React.useState(false);
+    var loaded = false;
     //Holds basic projects info including map ids, default data overwritten on async function
     const [projectInfo, setProjectInfo] = React.useState();
     //Holds specifics like results, locations, and types of markers, boundaries, etc.
@@ -1216,7 +1216,6 @@ function ProjectPage(){
                 collectionPoints(id, 'stationary', collection.date)
             ))
         ))*/
-        setLoaded(true);
     }, []);
 
     console.log(projectInfo)
@@ -1227,7 +1226,7 @@ function ProjectPage(){
             <TabPanel state={loc.state}/>
             {/* data passed into drawers needs map data and to match the format drawers component above */}
             {/* made it check for projectInfo.title before loading routes, later it will need to render on map data passed into drawers hopefully this helps */}
-            {loaded ? 
+            {projectMaps?.Results.boundaries !== {}/*projectInfo?.title*/ ? 
                 <Routes>
                     <Route index element={<MapPage title={ projectInfo?.title } 
                     drawers={{
