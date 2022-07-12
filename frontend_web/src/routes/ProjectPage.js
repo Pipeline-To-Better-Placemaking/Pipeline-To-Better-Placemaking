05ff49975a01e9fa770c1471dfd6a1a18cc55df0
Whisel <informer444@gmail.com>
2022-07-11 21:15:49 -0400

    removing second obj

--- frontend_web/src/routes/ProjectPage.js
+++ frontend_web/src/routes/ProjectPage.js
@@ -1230,18 +1230,18 @@ function ProjectPage(){
             { projectInfo?.title ?
                 <Routes>
                     <Route index element={<MapPage title={ projectInfo?.title } 
-                    drawers={{ results }} 
+                    drawers={ results } 
                         area={ projectInfo?.area.points } 
                         center={ projectInfo?.standingPoints } />} />
                     <Route path='map' element={<MapPage title={ projectInfo?.title } 
-                    drawers={{ results }} 
+                    drawers={ results } 
                         area={ projectInfo?.area.points } 
                         center={ projectInfo?.standingPoints }/>} />
                     <Route path='activities' element={<ActivityPage title={ projectInfo?.title }  
-                    drawers={{ results }} />} />
+                    drawers={ results } />} />
                     <Route path='activities/times' element={<NewActivityTimes />}/>
                     <Route path='surveyors' element={<SurveyorPage title={ projectInfo?.title } 
-                    drawers={{ results }} />} />
+                    drawers={ results } />} />
                 </Routes> 
                 : 
                 null
