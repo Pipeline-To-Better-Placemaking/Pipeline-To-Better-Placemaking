687009e4772d9f6ea6b0c802ae3881bba2c37d6d
ajayferd <alejandro.fernandez@knights.ucf.edu>
2023-02-12 11:47:43 -0500

    FINAL UPDATED CONFIG

--- utils/config.js
+++ utils/config.js
@@ -10,9 +10,16 @@ let CLIENT_SECRET = process.env.CLIENT_SECRET
 let REFRESH_TOKEN = process.env.REFRESH_TOKEN
 let ACCESS_TOKEN = process.env.ACCESS_TOKEN
 let GOOGLE_MAP_KEY = process.env.GOOGLE_MAP_KEY
+let TEST_DB_URI = process.env.TEST_DB_URI
 let JEST_TEST_URI = process.env.JEST_TEST_URI
 
-if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+
+if (process.env.NODE_ENV === 'dev') {
+	DB_URI = process.env.TEST_DB_URI
+    PRIVATE_KEY = process.env.TEST_PRIVATE_KEY
+}
+
+if (process.env.NODE_ENV === 'test') {
 	DB_URI = process.env.JEST_TEST_URI
     PRIVATE_KEY = process.env.TEST_PRIVATE_KEY
 }
@@ -27,5 +34,6 @@ module.exports = {
     REFRESH_TOKEN,
     ACCESS_TOKEN,
     GOOGLE_MAP_KEY,
+    TEST_DB_URI,
     JEST_TEST_URI
 }
