e2eba76d7455effe922223ed31669a502ed246f4
ajayferd <alejandro.fernandez@knights.ucf.edu>
2023-02-12 11:14:36 -0500

    updated some exporting and apis for section cutter

--- routes/teams.js
+++ routes/teams.js
@@ -10,6 +10,7 @@ const Nature_Map = require('../models/nature_maps.js')
 const Light_Map = require('../models/light_maps.js')
 const Order_Map = require('../models/order_maps.js')
 const Boundaries_Map = require('../models/boundaries_maps.js')
+const Section_Map = require('../models/section_maps.js')
 
 
 const passport = require('passport')
@@ -45,10 +46,11 @@ router.get('/:id', passport.authenticate('jwt',{session:false}), async (req, res
     for(var i = 0; i < team.users.length; i++){
         const user = await User.findById(team.users[i].user)
         myRole = team.users[i].role
-        team.users[i] = {user: user._id,
-                         role: myRole,
-                         firstname: user.firstname,
-                         lastname: user.lastname
+        team.users[i] = {
+                            user: user._id,
+                            role: myRole,
+                            firstname: user.firstname,
+                            lastname: user.lastname
                         }
     }
     res.status(200).json(team)
@@ -92,6 +94,7 @@ router.delete('/:id', passport.authenticate('jwt',{session:false}), async (req, 
                 await Light_Map.projectCleanup(proj)
                 await Order_Map.projectCleanup(proj)
                 await Boundaries_Map.projectCleanup(proj)
+                await Section_Map.projectCleanup(proj)
             }
     
             await Project.teamCleanup(team._id)
