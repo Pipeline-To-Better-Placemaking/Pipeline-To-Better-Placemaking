17c793e7c996cb7e2a3bfa9c6db2d0acb1518579
sebsours <sebsours@gmail.com>
2023-03-01 21:21:16 -0500

    Can store data from IP surveyor test now

--- frontend_web/src/routes/UnitySurveyorPage.js
+++ frontend_web/src/routes/UnitySurveyorPage.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useCallback, useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import Button from '@mui/material/Button';
-import { Link, useLocation, useNavigate} from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 
 export default function UnityPage() {
@@ -10,7 +10,7 @@ export default function UnityPage() {
   const nav = useNavigate();
   const [message, setMessage] = React.useState('');
   const [programJSON, setProgramJSON] = React.useState();
-  
+
   const {
     unityProvider,
     isLoaded,
@@ -22,7 +22,7 @@ export default function UnityPage() {
   } = useUnityContext({
     // This is just to make sure the file path is correct when loading Unity inside the website
     // Build loads in the activity path without ../ needs to load inside website root
-    loaderUrl: "../../../../../../../../Build/IPSurveyors.loader.js", 
+    loaderUrl: "../../../../../../../../Build/IPSurveyors.loader.js",
     dataUrl: "../../../../../../../../Build/IPSurveyors.data",
     frameworkUrl: "../../../../../../../../Build/IPSurveyors.framework.js",
     codeUrl: "../../../../../../../../Build/IPSurveyors.wasm",
@@ -46,14 +46,72 @@ export default function UnityPage() {
   }, [isLoaded])
 
   const handleSetProgramJSON = useCallback((programJSON) => {
-      // handle the JSON sent from Unity here
-      console.log(programJSON);
-      
+    // handle the JSON sent from Unity here
+    console.log(programJSON);
+    const obj = JSON.parse(programJSON);
 
-      // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MIGHT WANT TO CHANGE THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      // Where should we go after the surveyor submits a program? right now it goes back to the main
-      // project map page... which is probably where we should go back anyways.....
-      nav('../', { replace: true, state: {team: loc.state.team, project: loc.state.project, userToken: loc.state.userToken} });
+    let newFloors = [];
+
+    // create empty arrays for each floor
+    for (let i = 0; i < loc.state.data.data[0].numFloors; i++) {
+      newFloors.push([]);
+    }
+
+    // looping through the programs
+    for (let i = 0; i < obj.length; i++) {
+
+      console.log(obj[i]);
+
+      const newProgram = {
+        points: obj[i].points,
+        programType: obj[i].programType
+      };
+
+      console.log(newProgram);
+
+      // push newProgram into the appropriate floor array
+      newFloors[obj[i].floorNum].push(newProgram);
+
+    }
+
+    console.log(newFloors);
+
+    for (let i = 0; i < newFloors.length; i++) {
+      try {
+        const response = axios.put(`/program_floors/${loc.state.data.data[0].floors[i]}`, JSON.stringify({
+          programs: newFloors[i]
+
+        }), {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': `Bearer ${loc.state.userToken.token}`
+          },
+          withCredentials: true
+        });
+
+        // activityDetails = response.data;
+
+      } catch (error) {
+        console.log('ERROR: ', error);
+        // setMessage(error.response.data?.message);
+        // response.current.style.display = 'inline-block';
+        return;
+      }
+    }
+
+
+
+
+
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MIGHT WANT TO CHANGE THIS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Where should we go after the surveyor submits a program? right now it goes back to the main
+    // project map page... which is probably where we should go back anyways.....
+
+
+
+    // nav('../', { replace: true, state: {team: loc.state.team, project: loc.state.project, userToken: loc.state.userToken} });
 
   }, []);
 
@@ -64,18 +122,23 @@ export default function UnityPage() {
     };
   }, [addEventListener, removeEventListener, handleSetProgramJSON]);
 
-  async function handleSurveyors() 
-  {
+  async function handleSurveyors() {
     try {
-      const response = await axios.get(`/program_maps/63f8fdf8ac6cf91a9c1cb2e9`, {
-          headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-              'Authorization': `Bearer ${loc.state.userToken.token}`
-          },
-          withCredentials: true
+      console.log(loc.state.data);
+      const response = await axios.get(`/program_maps/${loc.state.data._id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Authorization': `Bearer ${loc.state.userToken.token}`
+        },
+        withCredentials: true
       });
-      console.log(response.data);
+
+      // console.log(response.data.programs.length);
+
+
+
+
 
       const obj = {
         numFloors: response.data.data[0].numFloors,
@@ -86,38 +149,38 @@ export default function UnityPage() {
       sendMessage("Building", "SurveyorProgram", myJSON);
 
     } catch (error) {
-        console.log('ERROR: ', error);
-        setMessage(error.response.data?.message);
-        // response.current.style.display = 'inline-block';
-        return;
+      console.log('ERROR: ', error);
+      setMessage(error.response.data?.message);
+      // response.current.style.display = 'inline-block';
+      return;
     }
-  
+
     // const obj = {
     //   numFloors: loc.state.numFloors,
     //   points: loc.state.buildingArea,
     // }
-      
+
     // const myJSON = JSON.stringify(obj)
     // // console.log(myJSON);
     // sendMessage("Building", "GetPoints", myJSON);
-  
+
   }
 
   return (
     <div>
-        <h1>Identifying Program</h1>
-        {/* state={{userToken:loc.state.userToken, team: loc.state.team}} <-- this is a parameter for the button component if you need it later*/}
-        
-        <div>
-            {!isLoaded && (
-              <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
-            )}
-            <Unity
-              unityProvider={unityProvider}
-              style={{ width: 1600, height: 900, visibility: isLoaded ? "visible" : "hidden" }}
-            />
+      <h1>Identifying Program</h1>
+      {/* state={{userToken:loc.state.userToken, team: loc.state.team}} <-- this is a parameter for the button component if you need it later*/}
 
-        </div>
+      <div>
+        {!isLoaded && (
+          <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+        )}
+        <Unity
+          unityProvider={unityProvider}
+          style={{ width: 1600, height: 900, visibility: isLoaded ? "visible" : "hidden" }}
+        />
+
+      </div>
     </div>
   );
 };
