4ddea087dc2454527830e14344ee07f4f0a4da18
scastaneda566 <sebastian.castaneda@knights.ucf.edu>
2023-03-07 15:23:22 -0500

    Upload Section Media now sends the title and gives image preview

--- frontend_web/src/routes/UploadSectionMedia.js
+++ frontend_web/src/routes/UploadSectionMedia.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { storage } from "./firebase";
+ import { storage } from "./firebase_config";
 import { ref, uploadBytesResumable, getDownloadURL, listAll, list } from "firebase/storage";
 import { v4 } from "uuid";
 import './routes.css';
@@ -10,33 +10,44 @@ import axios from '../api/axios';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function UploadSectionMedia() {
     const [mediaUrl, setMediaUrl] = useState(null);
     const [progresspercent, setProgresspercent] = useState(0);
     const [mediaUrls, setMediaUrls ] = useState([]);
+    const [title, setTitle ] = useState("");
+    const [titles, setTitles] = useState([]);
+    const [ image, setImage ] = useState("");
+    const [ uploaded, setUploaded ] = useState(false);
+    const [ loading, setLoading ] = useState(false);
     const location = useLocation();
     const date = new Date();
-    const storageRefList = ref(storage, `media_uploads/`);
+    const storageRefList = ref(storage, `media_uploads/${location.state.section._id}`);
+
 
     const handleSubmit = (e) => {
       e.preventDefault()
       const file = e.target[0]?.files[0]
       if (!file) return;
-      const storageRef = ref(storage, `media_uploads/${file.name + v4()}`);
+      const storageRef = ref(storage, `media_uploads/${location.state.section._id}/${file.name + v4()}`);
       const uploadTask = uploadBytesResumable(storageRef, file);
-      //var isoDate = new Date(`${activity.date}T00:00:00`)
       const storeURL = async (url) => {
           try {
-              console.log(url);
-              console.log(location.state.userToken);
-              const reponse = await axios.put(`/section_maps/${location.state.section._id}/data/${location.state.section.data[0]._id}`, JSON.stringify({
-                //date: isoDate.toISOString(),
-                url_link: url,
-              }));
+              await axios.post(`/section_maps/${location.state.section._id}/data`, JSON.stringify({
+                  url_link: url,
+                  title: title,
+                }), {
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': `Bearer ${location.state.userToken.token}`
+                  },
+                  withCredentials: true
+                });
           }
           catch (error) {
-            console.log("URL was not able to be stored on Mongo");
+            console.log("Data was not able to be stored on Mongo");
             return;
           }
       }
@@ -58,6 +69,17 @@ function UploadSectionMedia() {
           });
         }
       );
+    }
+
+    const handleImageLoad = () => {
+      setLoading(false);
+    }
+
+    const handleUpload = async (e) => {
+      setImage(URL.createObjectURL(e.target.files[0]));
+      setUploaded(true);
+      setLoading(true);
+
     }
 
     useEffect(() => {
@@ -84,32 +106,37 @@ function UploadSectionMedia() {
                 <div className="SubmitButton">
                   <h1> Section Cutter </h1>
                   <br></br>
-                  <input type='file' />
+                  <input type='file' onChange={handleUpload} />
                   <br></br>
                   <button type='submit'> Upload Media </button>
+                  <br></br>
+                  <progress id="file" max="100" value={progresspercent}>  </progress>
                 </div>
                 
             </form>
             <br></br>
-            <progress id="file" max="100" value={progresspercent}>  </progress>
-            <br></br>
+            {
+              image ? 
+              <div className="ImgPreview">
+                <br></br> 
+                <img style={{height: "20vh", width : "30vw"}} onLoad={handleImageLoad} src={image} />
+                <br></br>
+                <br></br>
+                <TextField label="Title"style={{width: "10vw", height: "15vh"}}onChange={text => {setTitle(text.target.value)}} value={title}></TextField>
+              </div>: null
+            }
             <Button className='continueButton' component={Link} size='lg' variant='filledTonal' color='success' to='../' 
               state={{...location.state}}>
               Accept and Continue
             </Button>
             <br></br>
-            {
-                  <div className="slide-container">
-                    <Slide>
-                      {mediaUrls.map((image, index)=> (
-                        <div key={index}>
-                          <div style={{ ...divStyle, 'backgroundImage': `url(${image})` }}>
-                          </div>
-                        </div>
-                      ))} 
-                    </Slide>
-                  </div>
-            }
+            {mediaUrls.map((url, index) => {
+              return (
+                <div className="MediaList">
+                  <img src={url} height="200px" width="300px" class="center"/>
+                </div>
+              );
+            })}
       </div>
     );
 }
