d765c6927ec1301a2c061fe1f19c62c8143cbf28
scastaneda566 <sebastian.castaneda@knights.ucf.edu>
2023-03-09 21:15:18 -0500

    Uploaded Tommy's tags

--- frontend_web/src/routes/UploadSectionMedia.js
+++ frontend_web/src/routes/UploadSectionMedia.js
@@ -14,17 +14,23 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel'
+import { MultipleSelect } from 'react-select-material-ui';
 
 function UploadSectionMedia() {
-    const [mediaUrl, setMediaUrl] = useState(null);
-    const [progresspercent, setProgresspercent] = useState(0);
-    const [mediaUrls, setMediaUrls ] = useState([]);
-    const [ image, setImage ] = useState("");
-    const [title, setTitle ] = useState("");
-    const [ uploaded, setUploaded ] = useState(false);
-    const [ loading, setLoading ] = useState(false);
-    const [selectedTag, setSelectedTag] = useState('');
+    const [ mediaUrl, setMediaUrl] = useState(null);
+    const [ progresspercent, setProgresspercent] = useState(0);
+    const [ mediaUrls, setMediaUrls ] = useState([]);
+    const [ preview, setPreview ] = useState("");
+    const [ title, setTitle ] = useState("");
+    const [ newTitle, setNewTitle ] = useState("");
+    const [ selectedTag, setSelectedTag ] = useState('');
+    const [ newSelectedTag, setNewSelectedTag ] = useState('');
     const [ tags, setTags ] = useState([]);
+    const [ newTags, setNewTags ] = useState([]);
+    const [ selectedSlide, setSelectedSlide ] = useState('');
+    const [ selectedIndex, setSelectedIndex ] = useState(0);
+    const [ edit, setEdit ] = useState(false);
+
 
     const location = useLocation();
     const date = new Date();
@@ -42,6 +48,7 @@ function UploadSectionMedia() {
               await axios.post(`/section_maps/${location.state.section._id}/data`, JSON.stringify({
                   url_link: url,
                   title: title,
+                  tags: tags,
                 }), {
                   headers: {
                     'Content-Type': 'application/json',
@@ -84,21 +91,54 @@ function UploadSectionMedia() {
       event.preventDefault();
       setTags([...tags, selectedTag]);
       setSelectedTag('');
-    };
-
-    const handleImageLoad = () => {
-      setLoading(false);
     }
 
-    const handleUpload = async (e) => {
-      setImage(URL.createObjectURL(e.target.files[0]));
-      setUploaded(true);
-      setLoading(true);
-
+    const handleNewTags = (event) => {
+      setNewSelectedTag(event.target.value);
     }
 
-    const handleCarousel = (index) => {
-      console.log(mediaUrls[index]);
+    const handleNewTagSubmit = (event) => {
+      event.preventDefault();
+      setNewTags([...newTags, newSelectedTag]);
+      setNewSelectedTag('');
+    }
+
+    const handlePreview = async (e) => {
+      setPreview(URL.createObjectURL(e.target.files[0]));
+    }
+
+    const handleSlide = (index) => {
+      setSelectedSlide(mediaUrls[index]);
+      setSelectedIndex(index);
+    }
+
+    const handleEdit = () => {
+      setEdit(true);
+    }
+
+    const handleSave = () => {
+      setEdit(false);
+      console.log(selectedIndex);
+      const update = async () => {
+        try {
+            await axios.put(`/section_maps/${location.state.section._id}/data/${location.state.section.data[selectedIndex + 1]._id}`, JSON.stringify({
+                title: newTitle,
+                tags: newTags,
+              }), {
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*',
+                  'Authorization': `Bearer ${location.state.userToken.token}`
+                },
+                withCredentials: true
+              });
+        }
+        catch (error) {
+          console.log("Data was not able to be updated on Mongo");
+          return;
+        }
+      }
+      update();
     }
 
     useEffect(() => {
@@ -111,13 +151,16 @@ function UploadSectionMedia() {
       });
       }, []);
 
+	const options = ["Sidewalk", "Transit Shelter", "Bus Lane", "Turn Lane", "Planter", "Drive Lane", "Bike Lane", "Parking Lane", "Street Lighting", "Stairs/Ramps", "Outdoor Seating Area", "Outdoor Restaurant Seating Area", "Canopy", "Building Structure", "Loggia", "Breezeway", "Drainage Field/Ditch", "Tree Canopy", "Lake/River Water", "Monument/Fountain", "Leisure Area"];
+
+
     return (
         <div className="UploadSectionMedia">
             <form onSubmit={handleSubmit} className='form'>
                 <div className="SubmitButton">
                   <h1> Section Cutter </h1>
                   <br></br>
-                  <input type='file' onChange={handleUpload} />
+                  <input type='file' onChange={handlePreview} />
                   <br></br>
                   <button type='submit'> Upload Media </button>
                   <br></br>
@@ -127,13 +170,13 @@ function UploadSectionMedia() {
             </form>
             <br></br>
             {
-              image ? 
+              preview ? 
               <div className="ImgPreview">
                 <br></br> 
-                <img style={{height: "20vh", width : "30vw"}} onLoad={handleImageLoad} src={image} />
+                <img style={{height: "20vh", width : "30vw"}} src={preview} />
                 <br></br>
                 <br></br>
-                <TextField label="Title"style={{width: "10vw", height: "15vh"}}onChange={text => {setTitle(text.target.value)}} value={title}></TextField>
+                <TextField label="Title"style={{width: "10vw", height: "15vh"}} onChange={text => {setTitle(text.target.value)}} value={title}></TextField>
                 <Box sx={{ minWidth: 120 }}>
                   <FormControl fullWidth>
                   <InputLabel id="demo-simple-select-label"> Tags </InputLabel>
@@ -144,9 +187,9 @@ function UploadSectionMedia() {
                     label="Tags"
                     onChange={handleTags}
                   >
-                  <MenuItem value={"House"}>House</MenuItem>
-                  <MenuItem value={"Bridge"}>Bridge</MenuItem>
-                  <MenuItem value={"Road"}>Road</MenuItem>
+                  {options.map((option, index) => (
+                      <MenuItem value={option}>{option}</MenuItem>
+                  ))}
                   </Select>
                   <Button onClick={handleTagSubmit}>Accept Tag</Button>
                   </FormControl>
@@ -161,14 +204,45 @@ function UploadSectionMedia() {
             <br></br>
             <div className="slide-container">
               <h3>Current Images</h3>
-              <Carousel showArrows={true} showThumbs={false} useKeyboardArrows={true} onChange={handleCarousel}>
+              <Carousel showArrows={true} showThumbs={false} useKeyboardArrows={true} onChange={handleSlide}>
                 {mediaUrls.map((slideImage, index)=> (
                   <div key={index}>
                     <img src={slideImage} height="300px" width="200px"/>
                   </div>
                 ))} 
               </Carousel>
-            </div> 
+            </div>
+            <Button onClick={handleEdit}>Open Edit Info</Button>
+            {
+              edit ?
+              <div className="Edit">
+                <TextField label="New Title"style={{width: "10vw", height: "15vh"}} value={newTitle} onChange={text => {setNewTitle(text.target.value)}}></TextField>
+                <Box sx={{ minWidth: 120 }}>
+                  <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label"> Tags </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={newSelectedTag}
+                    label="Tags"
+                    onChange={handleNewTags}
+                  >
+                  {options.map((option, index) => (
+                      <MenuItem value={option}>{option}</MenuItem>
+                  ))}
+                  </Select>
+                  <Button onClick={handleNewTagSubmit}>Accept Tag</Button>
+                  </FormControl>
+                  <ul>
+                    {newTags.map((newTag, index) => (
+                      <li key={index}>{newTag}</li>
+                    ))}
+                  </ul>
+                </Box>
+                
+                <Button onClick={handleSave}>Save</Button>
+              </div> : null
+            }
             <br></br>
             <Button className='continueButton' component={Link} size='lg' variant='filledTonal' color='success' to='../' 
               state={{...location.state}}>
